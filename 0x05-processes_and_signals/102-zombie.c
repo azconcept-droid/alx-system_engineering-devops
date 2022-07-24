c7eaf2e8091804b146ca22a0ad18ecad29a75e9b
Yahaya <azconcept2016@gmail.com>
2022-07-24 14:59:58 +0100

    :poop: zombie process

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+
+	return (0);
+}
+
+/**
+ * infinite_while - infinite while loop
+ *
+ * Return: 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
