a2ece8b441cd5be75bf430bd988cd198c4b3db2b
Yahaya <azconcept2016@gmail.com>
2022-09-30 12:40:00 +0100

    feat: automate using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,29 @@
+exec { 'update_ubuntu':
+  command =>     'apt-get update',
+  path    =>      ['/usr/bin/', '/usr/sbin'],
+}
+
+package { 'puppet':
+  ensure   =>    '5.5.10',
+  provider =>    'gem',
+}
+
+exec { 'install_nginx':
+  path    =>    ['/usr/bin/', '/usr/sbin'],
+  command =>    'apt-get install -y nginx',
+}
+
+exec { 'custom_body':
+  command =>      'echo "Hello World!" > /var/www/html/index.nginx-debian.html'
+  path    =>       ['/usr/bin/', '/usr/sbin'],
+}
+
+exec { 'redirect' :
+  command =>     "sed -i 's+_;+_;\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}+g' /etc/nginx/sites-available/default",
+  path    =>      ['/usr/bin/', '/usr/sbin'],
+}
+
+exec { 'restart_nginx':
+  command =>      'service nginx restart',
+  path    =>       ['/usr/bin/', '/usr/sbin'],
+}
