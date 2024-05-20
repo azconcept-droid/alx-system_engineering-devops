11894387a403f338d444b65255c29002b4e58b46
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-20 12:40:42 +0100

    get all tasks from all employees

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,46 @@
 #!/usr/bin/python3
-"""This module uses REST API, for a given employee ID,
-returns information about his/her TODO list progress
-in CSV format
+"""This module uses REST API, for a given user ID,
+records all tasks from all employees in JSON format
 """
+
 import requests
-import sys
+import json
 
 
 if __name__ == "__main__":
-    # Get employee id from command line
-    id = sys.argv[1]
-    # Assign user and todos endpoints or url to a variable
-    users = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-    todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
-    # Sending a get requests to each endpoints
-    response_users = requests.get(users)
+    # todos endpoints
+    todos = 'https://jsonplaceholder.typicode.com/todos?userId=1'
+    # Sending a get requests to endpoints
     response_todos = requests.get(todos)
     # Convert response to a json format
-    user = response_users.json()
     todos_list = response_todos.json()
 
-    name = user.get('username')
-    filename = "{}.json".format(user.get('id'))
-
-    # save data in csv format
-    with open(filename, mode='w') as file:
+    filename = "todo_all_employees.json"
+    # Prepare data
+    # { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+    # "completed": TASK_COMPLETED_STATUS},
+    # {"username": "USERNAME", "task": "TASK_TITLE",
+    # "completed": TASK_COMPLETED_STATUS}, ... ],
+    # "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+    # "completed": TASK_COMPLETED_STATUS},
+    # {"username": "USERNAME", "task": "TASK_TITLE",
+    # "completed": TASK_COMPLETED_STATUS}, ... ]}
+    data = []
+    for id in range(1, 11):
+        users = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+        response_users = requests.get(users)
+        user = response_users.json()
+        name = user.get('username')
         for todo in todos_list:
-            dict_list = []
             task_done = todo.get('completed')
             task_title = todo.get('title')
-            #"\"task\": \"{}\", \"completed\": {}, \"username\": \"{}\"".format(task_title, task_done, name)
-            dict_list.append(task_title)
-            #{"{}": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ...]}
-            file.write("{}".format(dict_list))
+
+            data.append({
+                "username": name,
+                "task": task_title,
+                "completed": task_done,
+            })
+
+    # Write the JSON data to a file
+    with open(filename, mode='w') as file:
+        json.dump({1: data}, file)
