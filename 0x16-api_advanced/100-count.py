78fee4f6db844f27d37a4b0d33dc0332c727350c
Yahaya Azeez <azconcept2016@gmail.com>
2024-06-04 17:16:37 +0100

    print keywords in sorted form

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module qqueries the Reddit API,
+parses the title of all hot articles,
+and prints a sorted count of given keywords
+"""
+
+
+def count_words(subreddit, word_list):
+    """ Print sorted count keywords """
+
+    print()
