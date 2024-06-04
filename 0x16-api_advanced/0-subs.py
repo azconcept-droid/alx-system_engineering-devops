2ec9df71581f9bec7446d87e9df75987987b5c55
Yahaya Azeez <azconcept2016@gmail.com>
2024-06-04 17:06:15 +0100

    get number of subscribbers in reddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module queries the Reddit API and
+returns the number of subscribers
+"""
+
+
+def number_of_subscribers(subreddit):
+    """ Reddit number of subscribers """
+
+    return 0
