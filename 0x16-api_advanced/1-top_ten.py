aab2f3faa180dee15acb3e78266b20b4bd1bfd1a
Yahaya Azeez <azconcept2016@gmail.com>
2024-06-04 17:09:00 +0100

    get top 10 posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module queries the Reddit API and
+prints the titles of the first 10 hot posts
+"""
+
+
+def top_ten(subreddit):
+    """ Reddit number of subscribers """
+
+    print("None")
