fef353fc931dcb13c967e23acaab46f50776bb9b
Yahaya Azeez <azconcept2016@gmail.com>
2024-06-04 17:13:07 +0100

    get hot titles

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and
+returns a list containing the titles of all hot articles
+"""
+
+
+def recurse(subreddit, hot_list=[]):
+    """ Get title of all hot articles """
+
+    return None
